2c4ef2f7106ed0ccb3f8e9c26f7a146ab247ae0f
vektorelpython <vektorelpythonibrahim@gmail.com>
2019-06-23 12:15:20 +0200

    pythonanywhere

--- BenimSite/settings.py
+++ BenimSite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#2$puivqloo8y1ppfp5lp@6=7l49y@-1a$0+5utt-w+gg%)le#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1","10.0.1.100"]
+ALLOWED_HOSTS = ["127.0.0.1","10.0.1.100","pythonanywhere.com"]
 
 
 # Application definition
