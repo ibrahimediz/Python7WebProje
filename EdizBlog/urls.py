31738458f8991e13b7339cb2dfddf18ff01df383
ibrahimediz <aaa@aaa.com>
2019-06-29 11:00:47 +0200

    template eklendi

---
+++ EdizBlog/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.gonderi_liste, name='gonderi_liste'),
+]
--- EdizBlog/views.py
+++ EdizBlog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
+def gonderi_liste(request):
+    return render(request, 'gonderi/gonderiliste.html', {})
 # Create your views here.
